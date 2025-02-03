9d0d3c43d12c96622908aaad6830efe466d632fb
pedkomaksim <pedkomaksim@gmail.com>
2025-02-03 17:05:17 +0200

    [add] empty project

---
+++ TestTask/TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Максим Педько on 03.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
